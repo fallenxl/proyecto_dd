badac3e499697f829285d4d6c1f1264f7116a95b
Fallen <axlsntz.dev@gmail.com>
2023-04-29 15:09:37 -0600

    upd

--- components/footer.php
+++ components/footer.php
@@ -1,8 +1,7 @@
 <footer class="bg-white shadow-sm py-4">
   <div class="container mx-auto">
     <h4 class="text-xl font-bold text-center">Anunciatee!</h4>
-    <p class="text-center">Todos los derechos reservados</p>
-
+    <p class="text-center"><strong>Fallen Software</strong> Todos los derechos reservados &#169 2023</p>
   </div>
 </footer>
 </body>
